f27f6ed4497a10158548ff8e40e2398668c1fdb8
Edwin Wang <lark930@gmail.com>
2020-05-24 12:23:03 +0800

    添加任务调度

--- app/Models/Auction.php
+++ app/Models/Auction.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class Auction extends Model
 {
@@ -52,15 +53,23 @@ class Auction extends Model
             'current_price' => $bidAmount,
         ]);
         // 释放已竞标的的锁定金额到各个竞标者的账户
-        $bids = $this->bids();
+        $bids = $this->bids()->where('locked', true)->get();
         foreach ($bids as $bid) {
-            $bid->user()->wallet()->unlock($bid['bid_price']);
+            $bid->user()->wallet()->unlock($bid['lock_amount']);
+            $bid->update([
+                'locked' => false,
+            ]);
+        }
+        // 检测用户金额是否大于 $bidAmount
+        if (User::findOrFail($userId)->wallet()->free_amount < $bidAmount) {
+            throw new BadRequestHttpException('Not enough free balance');
         }
         // 新增一条竞标数据
         $this->bids()->create([
             'user_id' => $userId,
             'bid_price' => $bidAmount,
             'bid_at' => now(),
+            'locked' => true,
         ]);
         // 锁定金额
         user::findOrFail($userId)->wallet()->lock($bidAmount);
@@ -77,13 +86,20 @@ class Auction extends Model
             'status' => Auction::STATUS_FIXED_SUCCESS,
         ]);
         // 释放已竞标的的锁定金额到各个竞标者的账户
-        $bids = $this->bids();
+        $bids = $this->bids()->where('locked', true)->get();
         foreach ($bids as $bid) {
-            $bid->user()->wallet()->unlock($bid['bid_price']);
+            $bid->user()->wallet()->unlock($bid['lock_amount']);
+            $bid->update([
+                'locked' => false,
+            ]);
+        }
+        // 检测用户金额是否大于 $fixedAmount
+        if (User::findOrFail($userId)->wallet()->free_amount < $fixedAmount) {
+            throw new BadRequestHttpException('Not enough free balance');
         }
         // 锁定金额
         user::findOrFail($userId)->wallet()->lock($fixedAmount);
-        // 创建订单
+        // TODO 创建订单
 
         DB::commit();
     }
@@ -100,5 +116,32 @@ class Auction extends Model
         $this->update([
             'status' => Auction::STATUS_BID_FAIL,
         ]);
+    }
+
+    public static function getExpiredAuctions() {
+        return Auction::where('status', Auction::STATUS_BIDDING)
+            ->where('end_at', '<=', now())->get();
+    }
+
+    public static function handleExpiredAuctions() {
+        DB::beginTransaction();
+        $expiredAuctions = self::getExpiredAuctions();
+        foreach ($expiredAuctions as $auction) {
+            $bids = $auction->bids();
+            if (count($bids) === 0) {
+                // 竞标失败
+                $auction->update([
+                    'status' => Auction::STATUS_BID_FAIL
+                ]);
+            } else {
+                // 竞标成功
+                $auction->update([
+                    'purchase_price' => $auction['current_price'],
+                    'status' => Auction::STATUS_BID_SUCCESS,
+                ]);
+                // TODO 创建订单
+            }
+        }
+        DB::commit();
     }
 }
