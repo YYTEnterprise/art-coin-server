c074231977de3a6588e0c97e6aa176c3fe7af479
Edwin Wang <lark930@gmail.com>
2020-05-28 10:02:15 +0800

    update

--- database/migrations/2020_05_20_010526_create_order_pays_table.php
+++ database/migrations/2020_05_20_010526_create_order_pays_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateOrderPayTable extends Migration
+class CreateOrderPaysTable extends Migration
 {
     /**
      * Run the migrations.
